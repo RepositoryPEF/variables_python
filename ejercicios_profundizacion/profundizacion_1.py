1c2c604a8c89922db50de443876b261cdc4d8c74
RepositoryPEF <109547083+RepositoryPEF@users.noreply.github.com>
2022-07-25 02:34:15 -0300

    CALCULADORA

--- ejercicios_profundizacion/profundizacion_1.py
+++ ejercicios_profundizacion/profundizacion_1.py
@@ -29,5 +29,34 @@ E) Exponente/Potencia
   ej: La suma entre 4.2 y 6.5 es 10.7
 '''
 
+from __future__ import division
+
+
 print('¡Nuestra primera calculadora!')
 # Empezar aquí la resolución del ejercicio
+
+print('Ingrese un numero Real:')
+numero1 = float(input())
+
+print('Ingrese otro numero Real:')
+numero2 = float(input())
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+potencia= numero1 ** numero2
+
+print(
+"La suma entre", numero1, "y", numero2, "es", suma
+)
+print(
+"La resta entre", numero1, "y", numero2, "es", resta
+)
+print(
+  "La multiplicacion entre", numero1, "y", numero2, "es", multiplicacion
+)
+print("La division entre", numero1, "y", numero2, "es", division
+)
+print(
+"La potencia entre", numero1, "y", numero2, "es", potencia
+)
